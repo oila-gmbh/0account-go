015765bb1d0ab0d4b9b03ab1c7cb9879ebbbb372
Kiura <m.k.x@mail.ru>
2024-03-05 18:12:50 +0300

    Add comment about userId

--- metadata.go
+++ metadata.go
@@ -10,6 +10,8 @@ func (m *Metadata) IsWebhookRequest() bool {
 	return m.isWebhookRequest
 }
 
+// UserID returns the user ID associated with the request.
+// Deprecated: it is not actually deprecated. The method is for internal use only.
 func (m *Metadata) UserID() string {
 	return m.userID
 }
